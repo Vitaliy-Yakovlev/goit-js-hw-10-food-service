9eeeeee1f6ebb4cb1c19bb2d213f51728357e34c
Vitaliy <vitaliy-yakovlev@ukr.net>
2021-05-09 23:30:56 +0300

    Добавил разметку в HTML

---
+++ src/js/light-dark.js
@@ -0,0 +1,17 @@
+import galeryItems from '../templates/galery-items.hbs';
+import menu from '../js/menu.json';
+import '../css/styles.css';
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const listContainer = document.querySelector('.js-menu');
+const listMarkup = createListMarkup(menu);
+
+listContainer.insertAdjacentHTML('beforeend', listMarkup);
+
+function createListMarkup(menu) {
+  return galeryItems(menu);
+}
